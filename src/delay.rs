f9ccc238f66c45b2c78218d867219c59e3916087
Raphael Nestler <raphael.nestler@gmail.com>
2018-05-29 16:26:06 +0200

    Implement Delay (#1)

---
+++ src/delay.rs
@@ -0,0 +1,45 @@
+use hal::blocking::delay::{DelayMs, DelayUs};
+
+use lpc11uxx::SYST;
+use cortex_m::peripheral::syst::SystClkSource;
+
+pub struct Delay {
+    syst: SYST,
+    clock_speed_mhz: u32,
+}
+
+impl Delay {
+    pub fn new(mut syst: SYST, clock_speed_hz: u32) -> Self {
+        syst.set_clock_source(SystClkSource::Core);
+        let clock_speed_mhz = (clock_speed_hz + 500_000) / 1_000_000;
+        Delay { syst, clock_speed_mhz }
+    }
+
+    pub fn free(self) -> SYST {
+        self.syst
+    }
+}
+
+impl DelayMs<u32> for Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        // TODO: may overflow
+        self.delay_us(ms * 1_000);
+    }
+}
+
+impl DelayUs<u32> for Delay {
+    fn delay_us(&mut self, us: u32) {
+        // TODO: may overflow
+        let rvr = us * self.clock_speed_mhz;
+
+        // systick is a 24bit timer
+        assert!(rvr < (1<<24));
+
+        self.syst.set_reload(rvr);
+        self.syst.clear_current();
+        self.syst.enable_counter();
+
+        while !self.syst.has_wrapped() {}
+        self.syst.disable_counter();
+    }
+}
