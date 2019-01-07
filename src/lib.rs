0c48e1bad54efa3534261126b9109b6dec6d5872
Danilo Bargen <mail@dbrgn.ch>
2019-01-07 22:45:59 +0100

    Rust 2018 edition (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,7 @@
+//! [Hardware Abstraction Layer](https://crates.io/crates/embedded-hal) (HAL)
+//! for NXP LPC11UXX family of µ-controllers.
 #![no_std]
 
-pub extern crate lpc11uxx;
-extern crate embedded_hal as hal;
-extern crate cortex_m;
+pub use lpc11uxx;
 
 pub mod delay;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
